43e2ef853fd97e28347e2db0cea053d2912f0ac4
성태 황 <sthwang@widerplanet.com>
2018-01-22 22:20:09 +0900

    테스트 채널 삭제

--- coin/alarm_pre.php
+++ coin/alarm_pre.php
@@ -5,8 +5,8 @@ require_once MAX_PATH . '/env.php';
 require_once MAX_PATH . '/conf.php';
 require_once MAX_PATH . '/controller/AlarmController.php';
 
-$instance = new AlarmController($GLOBALS['TELEGRAM_GROUP_ID']);
+$instance = new AlarmController($GLOBALS['TELEGRAM_CHANNEL_ID']);
 $instance->start();
 
-$instance2 = new AlarmController($GLOBALS['TELEGRAM_CHANNEL_ID']);
-$instance2->start();
+//$instance2 = new AlarmController($GLOBALS['TELEGRAM_GROUP_ID']);
+//$instance2->start();
