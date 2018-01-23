299deac9f876db7be69275bfc09fc40b3b27115d
성태 황 <sthwang@widerplanet.com>
2018-01-24 01:15:07 +0900

    거래소목록 추가

--- coin/binance_new.php
+++ coin/binance_new.php
@@ -5,10 +5,12 @@ require_once MAX_PATH . '/handle/Telegram.php';
 require_once MAX_PATH . '/env.php';
 require_once MAX_PATH . '/conf.php';
 require_once MAX_PATH . '/handle/UrlJsonParser.php';
+require_once MAX_PATH . '/service/Marketcap.php';
 
 $json_parser = UrlJsonParser::getInstance();
 $binance_json = $json_parser->getJsonToArray('https://www.binance.com/assetWithdraw/getAllAsset.html');
 $db = new mysqli($GLOBALS['database_host'], $GLOBALS['database_user'], $GLOBALS['database_password'], $GLOBALS['database_name']);
+$marketcap = new Marketcap();
 
 $sql = 'select id from binance_new';
 $sql_result = $db->query($sql)->fetch_all();
@@ -30,8 +32,9 @@ foreach ($coin_list as $list) {
     $list = strtolower($list);
     if (!array_search($list, $collected_coin)) {
 
-        $message = "## Binance new wallet ##\n*--------------*\n";
-        $message = $message.$list."\n\n*--------------*";
+        $message = "## Binance new wallet 🚀🚀🚀 ##\n*------------*\n";
+        $message = $message.$list."\n*------------*\n";
+        $message = $message."exchange list\n".implode(', ', $marketcap->get_markets($list));
 
         $telegram = new Telegram($GLOBALS['BOT_TOKEN'], $GLOBALS['TELEGRAM_CHANNEL_ID']);
         $telegram->telegramApiRequest("sendMessage", $message);
