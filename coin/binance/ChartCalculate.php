30658a6a779f48f161e4175fb68c9562e7de433e
성태 황 <sthwang@widerplanet.com>
2018-04-27 15:27:34 +0900

    조건 변경

--- coin/binance/ChartCalculate.php
+++ coin/binance/ChartCalculate.php
@@ -209,34 +209,34 @@ class ChartCalculate
             if ($i < 0) {
                 break;
             }
-            if (!$avgPrice[$size_of_avg_price] * 0.15 < $bolland[$i]) {
+            if (!$avgPrice[$size_of_avg_price] * 0.13 < $bolland[$i]) {
                 echo "NOT 행보 중\n";
                 return false;
             }
         }
 
-        if ($rsi_4h_avg * 0.45 > $rsi_4h &&
-            $rsi_1h_avg * 0.40 > $rsi_1h &&
-            $rsi_15m_avg * 0.40 > $rsi_15m) {
+        if ($rsi_4h_avg > $rsi_4h &&
+            $rsi_1h_avg * 0.50 > $rsi_1h &&
+            $rsi_15m_avg * 0.30 > $rsi_15m) {
             echo "1-1 pass\n";
         } else if ($rsi_1h_avg * 0.40 > $rsi_1h &&
-            $rsi_15m_avg * 0.40 > $rsi_15m) {
+            $rsi_15m_avg * 0.30 > $rsi_15m) {
             echo "1-2 pass\n";
-        } else if ($rsi_15m_avg * 0.35 > $rsi_15m) {
+        } else if ($rsi_15m_avg * 0.25 > $rsi_15m) {
             echo "1-3 pass\n";
         } else {
             echo "1 break\n";
             return false;
         }
 
-        if ($cci_4h_avg * 0.45 > $cci_4h &&
-            $cci_1h_avg * 0.40 > $cci_1h &&
-            $cci_15m_avg * 0.40 > $cci_15m) {
+        if ($cci_4h_avg > $cci_4h &&
+            $cci_1h_avg * 0.50 > $cci_1h &&
+            $cci_15m_avg * 0.30 > $cci_15m) {
             echo "2-1 pass\n";
         } else if ($cci_1h_avg * 0.40 > $cci_1h &&
-            $cci_15m_avg * 0.40 > $cci_15m) {
+            $cci_15m_avg * 0.30 > $cci_15m) {
             echo "2-2 pass\n";
-        } else if ($cci_15m_avg * 0.35 > $cci_15m) {
+        } else if ($cci_15m_avg * 0.25 > $cci_15m) {
             echo "2-3 pass\n";
         } else {
             echo "2 break\n";
